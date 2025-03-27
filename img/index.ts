d58d31d5816b0524285b0236ac32f2d9b3a4755d
余赟昊 <hope392163@163.com>
2025-03-27 10:05:31 +0800

    ✨ feat:
    连接钱包页面开发
    
    图片个别问题修复

--- img/index.ts
+++ img/index.ts
@@ -42,9 +42,23 @@ import nft_image_3 from "./nft-image-3.png";
 import musiceWave from "./musicwave.png";
 import update from "./update.png";
 import earn from "./eran.png";
-import NFTVideo from './nftvideo.png'
+import NFTVideo from "./nftvideo.png";
+import hero2 from "./hero2.png";
+import upload from "./upload.png";
+import provider1 from "./provider-1.png";
+import provider2 from "./provider-2.png";
+import provider3 from "./provider-3.png";
+import provider4 from "./provider-4.png";
 
 export default {
+  hero2,
+  provider1,
+  provider2,
+  provider3,
+  provider4,
+  upload,
+  NFTVideo,
+  earn,
   collection,
   update,
   musiceWave,
@@ -88,6 +102,4 @@ export default {
   vimeo,
   youtube,
   zgcmq,
-  earn,
-  NFTVideo
 };
